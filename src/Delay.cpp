95bc83bd747fbae9322709e337f22be043430ce8
Brent Haas <brent.haas@bugcrowd.com>
2017-04-13 00:04:01 -0700

    Added example usage

--- src/Delay.cpp
+++ src/Delay.cpp
@@ -12,3 +12,25 @@ unsigned long NonBlockDelay::Time(void)
  {
    return iTimeout;
  }
+
+/***** Usage ******
+#include "Delay.h"
+
+int led = 13;
+int ledstate = HIGH;
+NonBlockDelay d;
+
+void setup() {
+  pinMode(led, OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  if (d.Timeout()) {
+    digitalWrite(led, ledstate);
+    ledstate = ledstate == LOW ? HIGH : LOW; //toggle state
+    d.Delay(1000);
+  }
+  //Code here runs frequently
+}
+*/
